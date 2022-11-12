925791b29f5f37af9b6b73b1a4050db34b66038a
MariaBernadete Baroto <bernadetebaroto@gmail.com>
2022-11-12 13:41:52 +0800

    mplement option 2 & 3, while true loop implemented

--- Seatwork3_dict.py
+++ Seatwork3_dict.py
@@ -18,36 +18,46 @@ print("*******************************")
 Tracing_list = {
 }
 
+while True:
+    # Allow user to select item in the menu
+    choice1= input("Choose option displayed on menu(1-3): ")
 
-# Allow user to select item in the menu
-choice1= input("Choose option displayed on menu(1-3): ")
+    # Implement Option 1
+    if choice1.casefold() == "1" :
+        data1=input("What is your name? ")
+        data2=input("What is your age? ")
+        data3=input("What is your address? ")
+        data4=input("Please type your contact number: ")
+        data5=input("Do you experience any symptoms in the past 7 days?(y/n): ")
+        data6=input("In case of emergency, who will be your contact person? ")
+        data7=input("What is your relationship to your contact person? ")
+        data8=input("Please type the contact number of your contact person: ")
+        
+        dict_name = {
+            "Age"       : data2,
+            "Address"   : data3,
+            "Contact Number"  : data4,
+            "Symptoms" : data5,
+            "Contact Person" : data6,
+            "Relationship to Contact Person" : data7,
+            "Contact number of Contact Person" :data8
+        }
+        Tracing_list[data1] = dict_name
+        print(Tracing_list)
 
-
-# Implement Option 1
-if choice1.casefold() == "1" :
-    data1=input("What is your name? ")
-    data2=input("What is your age? ")
-    data3=input("What is your address? ")
-    data4=input("Please type your contact number: ")
-    data5=input("Do you experience any symptoms in the past 7 days?(y/n): ")
-    data6=input("In case of emergency, who will be your contact person? ")
-    data7=input("What is your relationship to your contact person? ")
-    data8=input("Please type the contact number of your contact person: ")
+    # Implement Option 2
+    if choice1.casefold() == "2":
+        search=input("Please enter the name: ")
+        search_dict=Tracing_list.get(search)
+        for key, value in search_dict.items():
+            print(key, ":", value)
     
-    name = {
-        "Full name" : data1,
-        "Age"       : data2,
-        "Address"   : data3,
-        "Contact Number"  : data4,
-        "Symptoms" : data5,
-        "Contact Person" : data6,
-        "Relationship to Contact Person" : data7,
-        "Contact number of Contact Person" :data8
-    }
-    Tracing_list.update(name)
-    for key, value in name.items():
-        print(key, ":", value)
+    if choice1.casefold() == "3":
+        choice=input("Are you sure you want to exit?(y/n)")
+        if choice == "y":
+            print("Thank you")
+            break
+        
 
-if choice1.casefold
-# Implement Option 2
+
 # Implement Option 3
