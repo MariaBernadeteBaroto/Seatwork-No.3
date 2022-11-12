4c2293546cf0cda18327a2a3f3651da1765f085c
MariaBernadete Baroto <bernadetebaroto@gmail.com>
2022-11-12 11:43:19 +0800

    Display menu

---
+++ Seatwork3_dict.py
@@ -0,0 +1,19 @@
+# Seatwork-3
+
+# Display a menu of options
+
+print("********************************")
+print()
+print("          MENU          ")
+print()
+print(" 1 -->  Add an item    ")
+print(" 2 -->  Search         ")
+print(" 3 -->  Exit (y/n)      ")  
+print()
+print()
+print("************************8******")
+
+# Allow user to select item in the menu
+# Implement Option 1
+# Implement Option 2
+# Implement Option 3
