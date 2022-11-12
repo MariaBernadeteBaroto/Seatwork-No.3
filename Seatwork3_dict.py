91d93eab16edb7c10539ac50f10d8e9d80219bd4
MariaBernadete Baroto <bernadetebaroto@gmail.com>
2022-11-12 12:34:50 +0800

    Implemented Option 1

--- Seatwork3_dict.py
+++ Seatwork3_dict.py
@@ -11,9 +11,38 @@ print(" 2 -->  Search         ")
 print(" 3 -->  Exit (y/n)      ")  
 print()
 print()
-print("************************8******")
+print("*******************************")
 
 # Allow user to select item in the menu
+choice1= input("Choose option displayed on menu(1-3): ")
+if choice1.casefold() == "1":
+    name=input("What is your name? ")
+    data2=input("What is your age? ")
+    data3=input("What is your address? ")
+    data4=input("Please type your contact number: ")
+    data5=input("Do you experience any symptoms in the past 7 days?(y/n): ")
+    data6=input("In case of emergency, who will be your contact person? ")
+    data7=input("What is your relationship to your contact person? ")
+    data8=input("Please type the contact number of your contact person: ")
+    
+    dict2 = {
+        "Full name" : name,
+        "Age"       : data2,
+        "Address"   : data3,
+        "Contact Number"  : data4,
+        "Symptoms" : data5,
+        "Contact Person" : data6,
+        "Relationship to Contact Person" : data7,
+        "Contact number of Contact Person" :data8
+    }
+
+    
+dict1 = {
+    name : dict2
+}
+
+print(dict1)
+
 # Implement Option 1
 # Implement Option 2
 # Implement Option 3
